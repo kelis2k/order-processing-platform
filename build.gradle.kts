d1b0d0823cbd856fd8d45f3813901c59ecd02cbb
drpot <n.potehin@>
2026-05-27 16:03:46 +0300

    fix: junit version in subprojects

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ subprojects {
     }
 
     dependencies {
+        "testImplementation"(platform("org.junit:junit-bom:5.10.2"))
         "testImplementation"("org.junit.jupiter:junit-jupiter")
         "testRuntimeOnly"("org.junit.platform:junit-platform-launcher")
     }
