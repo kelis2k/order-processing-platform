13a91141a6c6d1846a26b2eafbb35f658323bf25
drpot <n.potehin@>
2026-05-28 18:33:33 +0300

    build: add validation, springdoc, testcontainers deps to product-service

--- services/product-service/build.gradle.kts
+++ services/product-service/build.gradle.kts
@@ -7,6 +7,8 @@ dependencies {
     implementation(libs.spring.boot.starter.data.mongo)
     implementation(libs.spring.boot.starter.data.redis)
     implementation(libs.spring.boot.starter.actuator)
+    implementation(libs.spring.boot.starter.validation)
+    implementation(libs.springdoc.webmvc)
     implementation(libs.spring.kafka)
     implementation(libs.kafka.avro.serializer)
     implementation(libs.avro)
@@ -18,4 +20,18 @@ dependencies {
     compileOnly(libs.lombok)
     annotationProcessor(libs.lombok)
     annotationProcessor(libs.mapstruct.processor)
+    testImplementation(platform(libs.testcontainers.bom))
+    testImplementation(libs.testcontainers.junit)
+    testImplementation(libs.testcontainers.mongodb)
+    testImplementation(libs.spring.boot.starter.test)
+}
+
+tasks.withType<Test> {
+    environment("DOCKER_HOST", "tcp://localhost:2375")
+    systemProperty("DOCKER_API_VERSION", "1.47")
+    environment("TESTCONTAINERS_RYUK_DISABLED", "true")
+    environment("HTTP_PROXY", "")
+    environment("HTTPS_PROXY", "")
+    environment("http_proxy", "")
+    environment("https_proxy", "")
 }
